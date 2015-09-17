f81fb2dfdbc3ea79bfd532dcfdb46c2543422124
Maciej Kurek <maciejk77@gmail.com>
2015-09-17 14:28:07 +0100

    Connect middleware set up

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var connect = require('connect');
+var app = connect();
+app.listen(3000);
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  res.end('Hello World');
-}).listen(3000);
+// http.createServer(function(req, res) {
+  
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   });
+//   res.write('Hello World')
+//   res.end();
+// }).listen(3000);
 
 console.log('Server up and running at http://localhost:3000');
