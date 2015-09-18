4bf07e932ff632f833f29292531504ed8fa3364f
Maciej Kurek <maciejk77@gmail.com>
2015-09-18 09:06:50 +0100

    Simple Express server set up

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var connect = require('connect');
-var app = connect();
+app.use('/', function(req, res) {
+  res.send('Hello World');
+});
 
-var logger = function(req, res, next) {
-  console.log(req.method, req.url);
-  next();
-};
-
-var helloWorld = function(req, res, next) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-};
-
-var goodbyeWorld = function(req, res, next) {
-  res.setHeader('Content-Type', 'text-plain');
-  res.end('Goodbye World');
-};
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
 app.listen(3000);
+console.log('Server running at http://localhost:3000');
 
-console.log('Server up and running at http://localhost:3000');
+module.exports = app;
